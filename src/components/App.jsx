a08d91c95be28825fc068cb7e40b4c0b2b9c49eb
talka19 <talka19.10@gmail.com>
2023-03-05 02:45:29 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import {Profile} from 'components/Profile/profile.jsx';
+import user from 'components/Profile/user.json';
+// import data from './Statistics/data.json';
+// import {Statistics} from './Statistics/Statistics';
+// import friends from './FriendList/friends.json';
+// import {FriendList} from './FriendList/FriendList.jsx'
+// import transactions from './Transactions/ transactions.json';
+// import {TransactionHistory} from './Transactions/transactions.jsx'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+   <Profile
+   username={user.username}
+   tag={user.tag}
+   location={user.location}
+   avatar={user.avatar}
+   stats={user.stats}
+   />
+   {/* <Statistics title="Upload stats" stats={data} />
+   <Statistics stats={data} />
+   <FriendList friends={friends} />
+   <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
